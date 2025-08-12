c25fe480f2ffd738874c8055e2d9cba7e13acfa0
Guillaume Smet <guillaume.smet@gmail.com>
2025-08-12 11:33:05 +0200

    Modernize MFA extension

--- runtime/src/main/java/io/quarkus/mfa/runtime/MfaBuildTimeConfig.java
+++ runtime/src/main/java/io/quarkus/mfa/runtime/MfaBuildTimeConfig.java
@@ -1,46 +1,49 @@
 package io.quarkus.mfa.runtime;
 
-import io.quarkus.runtime.annotations.ConfigItem;
+import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithDefault;
 
-@ConfigRoot(name = "mfa")
-public class MfaBuildTimeConfig {
+@ConfigMapping(prefix = "quarkus.mfa")
+@ConfigRoot(phase = ConfigPhase.BUILD_AND_RUN_TIME_FIXED)
+public interface MfaBuildTimeConfig {
 
     /**
      * If the MFA extension is enabled.
      */
-    @ConfigItem(defaultValue = "true")
-    public boolean enabled;
+    @WithDefault("true")
+    boolean enabled();
 
     /**
      * The login view handler
      */
-    @ConfigItem(defaultValue = "/mfa_login")
-    public String loginView;
+    @WithDefault("/mfa_login")
+    String loginView();
 
     /**
      * The logout view handler
      */
-    @ConfigItem(defaultValue = "/mfa_logout")
-    public String logoutView;
+    @WithDefault("/mfa_logout")
+    String logoutView();
 
     /**
      * The login action handler
      */
-    @ConfigItem(defaultValue = "/mfa_action")
-    public String loginAction;
+    @WithDefault("/mfa_action")
+    String loginAction();
 
     /**
      * The landing page to redirect to if there is no saved page to redirect back to
      */
-    @ConfigItem(defaultValue = "/")
-    public String landingPage;
+    @WithDefault("/")
+    String landingPage();
 
     /**
      * Option to disable redirect to landingPage if there is no saved page to redirect back to. MFA POST is followed by redirect
      * to landingPage by default.
      */
-    @ConfigItem(defaultValue = "true")
-    public boolean redirectAfterLogin;
+    @WithDefault("true")
+    boolean redirectAfterLogin();
 
 }
